c707a93c580d8f2dc385e71056036b5a4de3b5fd
makualiyev <maku.aliyev@gmail.com>
2024-03-21 21:09:30 +0300

    staged

--- review_scraper/test_examply.py
+++ review_scraper/test_examply.py
@@ -2,10 +2,12 @@ import re
 from playwright.sync_api import Page, expect
 
 def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto("https://www.ozon.ru/")
+    expect(page).to_have_title(re.compile('Обновить'))
+    page.get_by_role("button", name="Обновить").click()
 
     # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
+    expect(page).to_have_title(re.compile("Варочная панель газовая DELVENTO V30H20J001 / 30 см / Газ-контроль / WOK-конфорка / Материал панели закалённое стекло / фронтальная панель управления / safety решетка / жиклеры для баллонного газа / полный комплект / 3 года гарантии"))
 
 def test_get_started_link(page: Page):
     page.goto("https://playwright.dev/")
